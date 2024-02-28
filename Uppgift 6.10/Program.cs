545762628750e4803e2945aaac890bec14681df6
Dijar06 <dize06001@utb.vaxjo.se>
2024-02-28 10:20:03 +0100

    Add project files.

---
+++ Uppgift 6.10/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en valfri sträng:");
+            string text = Console.ReadLine();
+            Console.WriteLine("Antal tal i texten: " + AntalTalIText(text));
+        }
+
+        static int AntalTalIText(string text)
+        {
+            string[] delar = text.Split(' ');
+            int antal = 0;
+
+            for (int i = 0; i < delar.Length; i++)
+            {
+                double tal;
+                if (double.TryParse(delar[i], out tal))
+                {
+                    antal++;
+                }
+            }
+            return antal;
+        }
+    }
+}
